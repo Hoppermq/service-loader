3537ae2acffd8e080bd44fa27d37f336a8681460
ZiXyos <killian.moudery@gmail.com>
2025-03-27 02:08:36 +0100

    feat: base config loader option

--- config/load.go
+++ config/load.go
@@ -2,23 +2,20 @@ package config
 
 import (
 	"embed"
+	"fmt"
 	"log/slog"
+	"strings"
 
+	"github.com/knadh/koanf/parsers/toml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/fs"
 	"github.com/knadh/koanf/v2"
 )
 
-// SecretLoader is an interface that can be implemented by the target to load secrets from SSM.
-type SecretLoader interface {
-	// Load loads a secret from the secret manager (e.g. aws ssm).
-	// The lookup secret path will be constructed as follows:
-	// /rancher2/{env}/{stack}/{serviceName}/{secretPath}
-	Load(intoKey string, secretPath string)
-}
-
-type ConfigSecretLoader interface {
-	SecretLoader
-	SetConfigLoader(configLoader *koanf.Koanf)
-	SetLogger(logger *slog.Logger)
+// PostLoad is for postloafing ahaha.
+type PostLoad interface {
+	PostLoad() error
 }
 
 type config struct {
@@ -26,9 +23,84 @@ type config struct {
   configLoader *koanf.Koanf
   fname  string
   fs *embed.FS
-  secretLoader SecretLoader
+  logger *slog.Logger
 }
 
-func (c *config)loader() error {
+
+func (c *config) loadFromFs() error {
+  if err := c.configLoader.Load(
+    fs.Provider(*c.fs, c.fname),
+    toml.Parser(),
+  ); err != nil {
+    return fmt.Errorf("error loading config file from fs: %w", err)
+  }
+
   return nil
+}
+
+func (c *config) loadFromLocal() error {
+  if err := c.configLoader.Load(
+    file.Provider(c.fname),
+    toml.Parser(),
+  ); err != nil {
+    return fmt.Errorf("error loading config file from file: %w", err)
+  }
+
+  return nil
+}
+
+func (c *config) loadFile() error {
+	if c.fname == "" {
+		return nil
+	}
+
+	if c.fs != nil {
+		return c.loadFromFs()
+	}
+
+	return c.loadFromLocal()
+}
+
+func (c *config) load() error {
+  c.configLoader = koanf.New(".")
+
+  if err := c.loadFile(); err != nil {
+		return fmt.Errorf("error loading file: %w", err)
+	}
+
+	if err := c.configLoader.Load(env.Provider("", ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(s), "_", ".")
+	}), nil); err != nil {
+		return fmt.Errorf("error loading env variables: %w", err)
+	}
+
+	if err := c.configLoader.Unmarshal("", c.target); err != nil {
+		return fmt.Errorf("error while unmarshalling env vars: %w", err)
+	}
+
+	if postLoad, ok := c.target.(PostLoad); ok {
+		if err := postLoad.PostLoad(); err != nil {
+			return fmt.Errorf("error while running post load: %w", err)
+		}
+	}
+
+  return nil
+}
+
+func Load(target interface{}, options ...Option) error {
+  conf := config{
+    target: target,
+  }
+
+  for _, opt := range options {
+    if err := opt(&conf); err != nil {
+      return err
+    }
+  }
+
+  if conf.logger == nil {
+    conf.logger = slog.Default();
+  }
+
+  return conf.load()
 }
